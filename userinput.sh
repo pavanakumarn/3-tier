18d2c2ff732882749e6ab47e38e3e96eadf5eac5
pavanakumarn <126378290+pavanakumarn@users.noreply.github.com>
2024-01-31 12:49:34 +0530

    Create userinput.sh

---
+++ userinput.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Please Provide your good name"
+read name
+echo "Please Provide your age"
+read age
+echo "Please Provide your city name"
+read city
+
+echo "Welcome to our shell script practice ${name},${age},${city}"
