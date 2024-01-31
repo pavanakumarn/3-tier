282df84c22723fd7c6b81cfa7cd6396cbe4f0317
pavanakumarn <126378290+pavanakumarn@users.noreply.github.com>
2024-01-31 22:34:04 +0530

    Create function.sh

---
+++ function.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FUNCTIONS : syntax is below
+
+function function_name() {
+  command.....
+}
+
+function args
+#################################################
+#!/bin/bash
+
+function welcome() {
+Name=$1
+echo "Welcome to $Name
+}
+
+welcome Pavan
+welcome Ram
+
+##################################################
