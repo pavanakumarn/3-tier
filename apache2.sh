6dea968b7d36674082566e8d6ad6d480578692d9
pavanakumarn <126378290+pavanakumarn@users.noreply.github.com>
2024-01-31 12:26:59 +0530

    Create apache2.sh

---
+++ apache2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo apt-get install #!/bin/bash
+
+sudo apt install apache2 -y
+sudo systemctl enable apache2
+sudo systemctl start apache2
+
+# To uninstall 
+# apt-get remove apache2
